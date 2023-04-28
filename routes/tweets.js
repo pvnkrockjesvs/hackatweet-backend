b1bac06d09bdae7189b5f7da5581a3e93bcbb5fd
Guillaume <ronnel.guillaume@proton.me>
2023-04-28 10:54:07 +0200

    routes LIKE + UNLIKE tweet

--- routes/tweets.js
+++ routes/tweets.js
@@ -42,7 +42,6 @@ router.delete('/', (req, res) => {
 router.post('/like', (req, res) => {
    Tweet.updateOne({_id : req.body.id}, {$inc: {'likes' : 1}}).then(data => {
       res.json({ data})
-
    })
 })
 
